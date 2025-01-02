cbece65794a2837603acaa4d7887c9d7212e09bd
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2025-01-02 02:16:10 +0100

    first commit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,29 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        plugin = {
+                "pretty", //console renkli yazdirmak icin
+                "html:target/default-cucumber-reports.html", //html report için
+                "json:target/json-reports/cucumber.json",  //plugin ekledikten sonra bunu cucumber report için ekle
+                "junit:test-output/htmlReport/cucumber.xml", //xml report için
+                "rerun:TestOutput/failed_scenario.txt"},//bu satirdaki kod, hata veren testleri kaydedip yeniden otomatik kosmak icin
+        monochrome = true,
+        features ="./src/test/resources/features",//Birden fazla features kosturmak icin {"features1","features2"} seklinde yazariz
+        glue = {"stepdefinitions","hooks"},
+        /* 1-Bu parametre ile kodlarımızı yazdığımız stepDefinition class'ının packege'ını belirtiriz
+           2- Hooks classin yolunu veririz.
+        */
+        tags = "@navigations",
+        dryRun = false
+        //console okunakli hale getirir,false scenario adimlarini renkli gosterir,  true renksiz yapar
+)
+
+public class Runner {
+
+}
---
+++ src/test/resources/features/01_navigations.feature
@@ -0,0 +1,7 @@
+@navigations
+Feature: navigations
+
+
+  Scenario: navigations test
+
+    Given I navigate to "https://google.com"
